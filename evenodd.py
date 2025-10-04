bbf8d634655294ec08c4138404326d7aa5339cb1
Jay Thakur <jayt0667@gmail.com>
2025-10-04 12:50:54 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,8 @@
+def evenodd(iput): 
+    if iput%2==0:
+        print("The Given Number is Even.")
+    else:
+        print("The Given Nunmber is Odd.")
+
+iput = int(input("Enter Your Number:"))
+evenodd(iput)
